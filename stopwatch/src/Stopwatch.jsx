e4f454594cfb3c6ff63ebc5ae5cc524b5fa485d6
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-04-04 08:01:30 +0545

    Add Stopwatch component and integrate into App

---
+++ stopwatch/src/Stopwatch.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect, useRef, use } from 'react'
+
+const Stopwatch = () => {
+    const [isRunning, setIsRunning] = useState(false);
+    const [elsapsedTime, setElapsedTime] = useState(0);
+    const intervalIdRef = useRef(null);
+    const startTimeRef = useRef(0);
+
+    useEffect(() => {
+    }, [isRunning]);
+
+    function start() {
+
+    }
+
+    function stop() {
+
+    }
+
+    function reset() {
+
+    }
+    function formatTime() {
+        return `00:00:00`;
+    }
+
+    return (
+        <div className='stopwatch'>
+            <div className='display'>{formatTime()}</div>
+
+        </div>
+    )
+}
+
+export default Stopwatch
