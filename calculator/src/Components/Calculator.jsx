e14ea73511658137714dcbca74927d1262fee8a7
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-04-06 16:24:38 +0545

    feat: initialize dark/light mode application with React, Vite, and Tailwind CSS
    - Added package.json with dependencies for React, Vite, and Tailwind CSS.
    - Created postcss.config.js for PostCSS configuration.
    - Added Vite logo SVG to public directory.
    - Created App component with dark/light mode functionality.
    - Implemented DarkAndLIghtMode component to toggle between modes.
    - Added Tailwind CSS styles in index.css.
    - Configured Tailwind CSS in tailwind.config.js.
    - Set up Vite configuration in vite.config.js.
    - Created main entry point for the application.

--- calculator/src/Components/Calculator.jsx
+++ calculator/src/Components/Calculator.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-function Calculator() {
+const Calculator= () => {
     const [result, setResult] = useState("");
 
     const clickHandle = (e) =>{
---
+++ dark_light_mode/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import DarkAndLIghtMode from './Components/DarkAndLIghtMode'
+
+function App() {
+  return (
+   <div className="App">
+    <DarkAndLIghtMode />
+    </div>
+  )
+}
+
+export default App
