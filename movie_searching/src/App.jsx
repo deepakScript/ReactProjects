68d51b2f8d4b0867af2176bb86db5569ceaef347
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-04-07 09:03:05 +0545

    feat: add Navbar, SearchBar, and MovieCard components to enhance application layout

--- movie_searching/src/App.jsx
+++ movie_searching/src/App.jsx
@@ -1,10 +1,16 @@
 
 import './App.css'
+import MovieCard from './Components/MovieCard'
+import Navbar from './Components/Navbar'
+import SearchBar from './Components/SearchBar'
 
 function App() {
   
   return (
     <>
+    <Navbar />
+    <SearchBar />
+    <MovieCard />
     </>
   )
 }
---
+++ movie_searching/src/Components/MovieCard.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const MovieCard = () => {
+  return (
+    <div>
+      <div className='main flex px-4 flex-wrap px-4 lg:px-10'>
+        <div className=' child p-2 lg:w-1/4 w-full '>
+            <div className=' sub-child bg-[#002e4b] p-3  rounded-2xl'>
+            <img className='w-full rounded-lg mb-2' src="https://www.dummyimage.com/720x400/000/fff.png" alt="image" />
+            <h2 className='text-white text-xl font-bold'>Thhis is title</h2>
+            <h2 className='text-white text-lg mb-2'>This is desc</h2>
+            </div>
+        </div>
+        <div className=' child p-2 lg:w-1/4 w-full '>
+            <div className=' sub-child bg-[#002e4b] p-3  rounded-2xl'>
+            <img className='w-full rounded-lg mb-2' src="https://www.dummyimage.com/720x400/000/fff.png" alt="image" />
+            <h2 className='text-white text-xl font-bold'>Thhis is title</h2>
+            <h2 className='text-white text-lg mb-2'>This is desc</h2>
+            </div>
+        </div>
+        <div className=' child p-2 lg:w-1/4 w-full '>
+            <div className=' sub-child bg-[#002e4b] p-3  rounded-2xl'>
+            <img className='w-full rounded-lg mb-2' src="https://www.dummyimage.com/720x400/000/fff.png" alt="image" />
+            <h2 className='text-white text-xl font-bold'>Thhis is title</h2>
+            <h2 className='text-white text-lg mb-2'>This is desc</h2>
+            </div>
+        </div>
+        <div className=' child p-2 lg:w-1/4 w-full '>
+            <div className=' sub-child bg-[#002e4b] p-3  rounded-2xl'>
+            <img className='w-full rounded-lg mb-2' src="https://www.dummyimage.com/720x400/000/fff.png" alt="image" />
+            <h2 className='text-white text-xl font-bold'>Thhis is title</h2>
+            <h2 className='text-white text-lg mb-2'>This is desc</h2>
+            </div>
+        </div>
+        <div className=' child p-2 lg:w-1/4 w-full '>
+            <div className=' sub-child bg-[#002e4b] p-3  rounded-2xl'>
+            <img className='w-full rounded-lg mb-2' src="https://www.dummyimage.com/720x400/000/fff.png" alt="image" />
+            <h2 className='text-white text-xl font-bold'>Thhis is title</h2>
+            <h2 className='text-white text-lg mb-2'>This is desc</h2>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default MovieCard
