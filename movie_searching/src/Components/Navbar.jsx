b475d3a63f917918a15def34f6790712b8c42529
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-04-07 09:42:14 +0545

    feat: update Navbar component title from "Movie Masala" to "Deep Movie"

--- movie_searching/src/Components/Navbar.jsx
+++ movie_searching/src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className='main lg:flex md:flex flex-wrap justify-between items-center px-4 bg-[#40407a] py-4 shadow-md sticky top-0'>
         <div className='left flex items-center space-x-3 justify-center'>
             <img className='w-10' src="https://movie-app-88kamal.vercel.app/clipart3105859.png" alt="img" />
-            <h1 className='font-bold text-2xl text-white'>Movie Masala</h1>
+            <h1 className='font-bold text-2xl text-white'> Deep Movie</h1>
         </div>
         <div className='right'>
             <ul className='flex space-x-5 text-white justify-center'> 
