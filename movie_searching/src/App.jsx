46a69081457f08c7cd1e3d40e45145e9057faa8a
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-04-07 09:15:47 +0545

    feat: implement movie data fetching and enhance layout with background styling

--- movie_searching/src/App.jsx
+++ movie_searching/src/App.jsx
@@ -1,16 +1,34 @@
-
+import React,{ useState } from 'react'
 import './App.css'
 import MovieCard from './Components/MovieCard'
 import Navbar from './Components/Navbar'
 import SearchBar from './Components/SearchBar'
 
 function App() {
-  
+  const [allMovieData, setAllMovieData] = useState([]);
+  const [searchMovie, setSearchMovie] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchMovieData = async () => {
+  try {
+    setLoading(true);
+    const res = await fetch(`https://www.omdbapi.com/?s=${searchMovie}&apikey=3568dab`);
+    const data = await res.json();
+    setAllMovieData(data);
+    setLoading(false);
+  } catch (error) {
+    setLoading(false);
+    console.log(error);
+    
+  }
+  }
   return (
     <>
-    <Navbar />
-    <SearchBar />
-    <MovieCard />
+      <Navbar />
+      <div className='bg'>
+        <SearchBar searchMovie={searchMovie} setSearchMovie={setSearchMovie}  />
+        <MovieCard allMovieData={allMovieData} loading={loading} />
+      </div>
     </>
   )
 }
