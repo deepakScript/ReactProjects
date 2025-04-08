0752f095ab45e1ae176baebc40d68e77974ccf7c
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-04-08 15:07:53 +0545

    feat: refactor App, Searchbar, and ProductCard components for improved functionality and styling

--- foodsearchingApp/src/App.jsx
+++ foodsearchingApp/src/App.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 
 import Navbar from './Components/Navbar'
 import Searchbar from './Components/Searchbar'
@@ -8,27 +8,30 @@ function App() {
   const [allFood, setAllFood] = useState([]);
   const [search, setSearch] = useState('');
   const [loading, setLoading] = useState(false);
+  const APP_ID = "b3972793"
+  const APP_KEY = "91d4d909be31b7f05b7997fb6f2380b0"
 
-  const fetchFood = async() => {
-    const appID = '5859bc55';
-    const appkey = '9df790de5e5e2d671b6f29604181960f';
-    try{
-      const res = await fetch(`https://api.edamam.com/api/recipes/v2?type=user&q=${search}&app_id=${appID}&app_key=${appkey}`);
+  const fetchFood = async () => {
+    try {
+      setLoading(true)
+      const res = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${search}&app_id=${APP_ID}&app_key=${APP_KEY}`);
       const data = await res.json();
-      setAllFood(data.hits)
+      setAllFood(data.hits);
       console.log(data.hits);
-    }catch{
-
+      
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
     }
   }
 
   return (
     <div>
       <Navbar />
-      <Searchbar />
-      <ProductCard />
+      <Searchbar search={search} setSearch={setSearch} fetchFood={fetchFood} />
+      <ProductCard allFood={allFood} loading={loading} />
 
-      <button onClick={fetchFood}>Search food</button>
     </div>
   )
 }
