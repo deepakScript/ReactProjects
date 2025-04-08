c870e83d1cbf44f5b4c96be9c3a449917a323401
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-04-08 14:46:45 +0545

    feat: implement food fetching functionality and update App component structure

--- foodsearchingApp/src/App.jsx
+++ foodsearchingApp/src/App.jsx
@@ -1,16 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import React,{useState} from 'react'
+
 import Navbar from './Components/Navbar'
 import Searchbar from './Components/Searchbar'
 import ProductCard from './Components/ProductCard'
 
 function App() {
+  const [allFood, setAllFood] = useState([]);
+  const [search, setSearch] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const fetchFood = async() => {
+    const appID = '5859bc55';
+    const appkey = '9df790de5e5e2d671b6f29604181960f';
+    try{
+      const res = await fetch(`https://api.edamam.com/api/recipes/v2?type=user&q=${search}&app_id=${appID}&app_key=${appkey}`);
+      const data = await res.json();
+      setAllFood(data.hits)
+      console.log(data.hits);
+    }catch{
+
+    }
+  }
 
   return (
     <div>
       <Navbar />
       <Searchbar />
       <ProductCard />
+
+      <button onClick={fetchFood}>Search food</button>
     </div>
   )
 }
