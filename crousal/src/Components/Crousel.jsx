219d2a05a055a9f4010f4892e70d7ac4f7319bf7
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-04-09 07:01:48 +0545

    feat: add initial Crousel component with image array and state management

---
+++ crousal/src/Components/Crousel.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+const images = [
+    "https://ecommerce-1-0-main-frontend.vercel.app/img/mainbanner1-1680x900.jpg",
+    "https://ecommerce-1-0-main-frontend.vercel.app/img/mainbanner2-1680x900.jpg",
+    "https://ecommerce-1-0-main-frontend.vercel.app/img/mainbanner1-1680x900.jpg",
+    "https://ecommerce-1-0-main-frontend.vercel.app/img/mainbanner2-1680x900.jpg",
+    "https://ecommerce-1-0-main-frontend.vercel.app/img/mainbanner1-1680x900.jpg"
+];
+
+const Crousel = () => {
+    const [current, setCurrent] = useState(0);
+
+    const nextSlide = () => {
+        setCurrent(current === )
+    }
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Crousel
