810526075d3ed10e4dbd3780c8d823c0c590c7cb
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-04-20 20:58:25 +0545

    feat: add Swiper integration and create Item and Title components for New Arrivals section

--- e-commercefullstack/frontend/src/components/NewArrivals.jsx
+++ e-commercefullstack/frontend/src/components/NewArrivals.jsx
@@ -1,10 +1,75 @@
-import React from 'react'
+import Title from './Title'
+
+import React, { useEffect, useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import {products} from '../assets/data';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+import Item from '../components/Item';
 
 const NewArrivals = () => {
+  const [newArrivals, setNewArrivals] = useState([]);
+
+  useEffect(()=>{
+    const data = products.slice(0, 10);
+    console.log(data);
+    
+    setNewArrivals(data);
+  },[products])
+
   return (
-    <div>
-      new Arrivals
-    </div>
+    <section>
+      <Title />
+      {/* sweeper container */}
+
+      <Swiper
+        autoplay={{
+          delay: 3500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          400:{
+          slidesPerView: 2,
+          sapceBetween: 30,
+        },
+        700:{
+          slidesPerView: 3,
+          sapceBetween: 30,
+        },
+        1024:{
+          slidesPerView: 4,
+          sapceBetween: 30,
+        },
+        1200:{
+          slidesPerView: 5,
+          sapceBetween: 30,
+        }
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination]}
+        className="h-[555px]"
+      >
+        {newArrivals.map((product) => (
+          <SwiperSlide key={product._id}>
+            <Item product={product} />
+          </SwiperSlide>
+          ))}
+        
+      </Swiper>
+    </section>
   )
 }
 
