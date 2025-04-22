de205d26c46823af6fd2ac96cd6e0a70adb11d03
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-04-23 02:54:59 +0545

    feat: enhance Collection page with filtering and sorting functionality

--- e-commercefullstack/frontend/src/pages/Collection.jsx
+++ e-commercefullstack/frontend/src/pages/Collection.jsx
@@ -1,23 +1,122 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import ShowSearch from '../components/ShowSearch'
+import { ShopContext } from '../context/ShopContext'
+import Product from './Product';
 
 const Collection = () => {
+  const { products, search, showsearch } = useContext(ShopContext);
+  const [category, setCategory] = useState([]);
+  const [subCategory, setSubCategory] = useState([]);
+  const [sortType, setSortType] = useState('relevant');
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemPerPage = 12;
+
+
+  const toggleFilter = (value, setState) => {
+    setState((prev) =>
+      prev.includes(value) ? prev.filter((item) => item !== value) : [...prev, value]
+    );
+  };
+
+  const applyFilters = () => {
+    let filtered = [...products];
+
+    if (search && showsearch) {
+      filtered = filtered.filter((Product) =>
+        Product.name.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+
+    if (category.length > 0) {
+      filtered = filtered.filter((Product) => category.includes(Product.category));
+    }
+
+    if (subCategory.length > 0) {
+      filtered = filtered.filter((Product) => subCategory.includes(Product.subCategory));
+    }
+
+    return filtered;
+  }
+
+  const applySorting = (productList) => {
+    switch (sortType) {
+      case "low":
+        return productList.sort((a, b) => a.price - b.price);
+      case "high":
+        return productList.sort((a, b) => b.price - a.price);
+      default:
+        return productList
+    }
+  }
+
+
+  useEffect(()=>{
+    let filtered = applyFilters();
+    let sorted = applySorting(filtered)
+
+    setFilteredProducts(sorted)
+    setCurrentPage(1)
+
+  }, [category, subCategory, sortType, products, search, showsearch])
+
+
+  const getPaginationProducts = () => {
+    const startIndex = (currentPage -1 ) * itemsPerPage
+    const endIndex = startIndex + itemPerPage
+    return filteredProducts.slice(startIndex, endIndex)
+  }
+
+  const totalPages = Math.ceil(filteredProducts.length / itemPerPage)
   return (
     <div className='max-padd-container !px-0'>
       <div className='flex flex-col sm:flex-row gap-8 mb-16 '>
         <div className='min-w-72 bg-primary p-4 pl-6 rounded-xl
         lg:pl-12 rounder-r-xl'>
           <ShowSearch />
-          <div>
-            <h5>Categories</h5>
-            <div>
-              {["Men", "Women", "kids"].map((cat) =>(
-                <label key={cat} className='flex items-center gap-x-2'>
+          <div className='pl-5 py-3 mt-6 bg-white rounded-xl'>
+            <h5 className='h5 mb-4'>Categories</h5>
+            <div className='flex flex-col gap-2 text-sm font-light'>
+              {["Men", "Women", "kids"].map((cat) => (
+                <label key={cat} className='flex gap-2 medium-14 textgray-500'>
                   <input type="checkbox" value={cat}
-                  className='w-3' />
-                  </label>
+                    className='w-3'
+                    onChange={(e) => toggleFilter(e.target.value, setCategory)} />
+                  {cat}
+                </label>
               ))}
             </div>
+          </div>
+          <div className='pl-5 py-3 mt-6 bg-white rounded-xl'>
+            <h5 className='h5 mb-4'>Types</h5>
+            <div className='flex flex-col gap-2 text-sm font-light'>
+              {["TopWear", "BottomWear", "FootWear"].map((subCat) => (
+                <label key={subCat} className='flex gap-2 medium-14 text-gray-30'>
+                  <input type="checkbox" value={subCat}
+                    className='w-3'
+                    onChange={(e) => toggleFilter(e.target.value, setSubCategory)} />
+                  {subCat}
+                </label>
+              ))}
+            </div>
+          </div>
+          <div className='px-4 py-3 mt-6 bg-white rounded-xl'>
+            <h5 className='h5 mb-4'>Sort By</h5>
+            <select onChange={(e) => setSortType(e.target.value)}
+              className='border border-slate-900/5 outline-none text-gray-30 medium-14
+            h-8 w-full rounded px-2'
+              name="" id="">
+              <option value="relevent">Relevent</option>
+              <option value="low">Low</option>
+              <option value="high">High</option>
+            </select>
+          </div>
+        </div>
+
+        {/* right side */}
+        <div>
+          <div>
+
           </div>
         </div>
       </div>
