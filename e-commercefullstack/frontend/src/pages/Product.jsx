253695f60425fc7e36a375071a2859d44e04bea9
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-04-23 03:28:12 +0545

    feat: refactor Product component to utilize ShopContext and fetch product data

--- e-commercefullstack/frontend/src/pages/Product.jsx
+++ e-commercefullstack/frontend/src/pages/Product.jsx
@@ -1,6 +1,38 @@
-import React from 'react'
-
+import React, { use, useEffect } from 'react'
+import { createContext, useContext, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext'
+import { FaStar, FaStarHalfAlt, FaTruck } from 'react-icons/fa'
+import { FaHeart } from 'react-icons/fa'
+import { TbShoppingBagPlus } from 'react-icons/tb'
 const Product = () => {
+
+  const [productId] = useParams();
+  const {products, currency} = useContext(ShopContext)
+  const [product, setProduct] = useState(null)
+  const [image, setImage ] = useState("")
+  const [size, setSize] = useState("")
+
+
+  const fetchProductData = async () =>{
+    const selectProduct = products.fint((item) => item._id === productID)
+
+    if(selectProduct){
+      setProduct(selectProduct)
+      setImage(selectProduct.Image[0])
+      console.log(selectProduct);
+      
+    }
+  }
+
+  useEffect(()=>{
+    fetchProductData()
+
+  },[productId, products])
+
+  if(!product){
+    return <div>.....Loading</div>
+  }
   return (
     <div>
       <h1>Product</h1>
